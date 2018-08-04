f8ed7046fbdcf7ba4026eaffae74344928410810
ozra <petar.ozretic@mail.com>
2018-08-04 09:48:57 +0200

    Implement form functionality

--- src/index.js
+++ src/index.js
@@ -39,15 +39,49 @@ class Button extends React.Component {
 class Forma extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: null
-    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    if (e.target.name === "title") {
+      this.props.onTitleChange(e.target.value);
+    }
+    if (e.target.name === "author") {
+      this.props.onAuthorChange(e.target.value);
+    }
+    if (e.target.name === "pages") {
+      this.props.onPagesChange(e.target.value);
+    }
+    if (e.target.name === "read") {
+      this.props.onStatusChange(e.target.value);
+    }
   }
   render() {
     return (
       <form className="forma">
         <h2>Insert new book</h2>
-        <input type="text" />
+        <label>
+          Title: <br />
+          <input name="title" type="text" onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          Author: <br />
+          <input name="author" type="text" onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          Pages:
+          <input name="pages" type="number" onChange={this.handleChange} />
+        </label>
+
+        <label>
+          Status:
+          <input name="read" type="checkbox" onChange={this.handleChange} />
+        </label>
+        <br />
+        <div className="button mali" onClick={() => this.props.onClick()}>
+          Add{" "}
+        </div>
       </form>
     );
   }
@@ -77,16 +111,48 @@ function Card(props) {
 class Library extends React.Component {
   constructor(props) {
     super(props);
-    this.renderForm = this.renderForm.bind(this);
     this.state = {
       button: false,
-      newBook: {
-        title: "",
-        author: "",
-        pages: null,
-        read: false
-      }
+      library: books.slice(),
+      title: "",
+      author: "",
+      pages: null,
+      read: false
     };
+    this.renderForm = this.renderForm.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleAuthorChange = this.handleAuthorChange.bind(this);
+    this.handlePagesChange = this.handlePagesChange.bind(this);
+    this.handleStatusChange = this.handleStatusChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit() {
+    let newBook = {
+      title: this.state.title,
+      author: this.state.author,
+      pages: this.state.pages,
+      status: this.state.read
+    };
+
+    const helper = this.state.library.slice().concat([newBook]);
+    this.setState({ library: helper });
+    let forma = document.querySelector(".transition");
+
+    forma.classList.remove("transition");
+
+    /*TU RADIMO*/
+  }
+  handleStatusChange(value) {
+    this.setState({ read: value });
+  }
+  handlePagesChange(value) {
+    this.setState({ pages: value });
+  }
+  handleAuthorChange(value) {
+    this.setState({ author: value });
+  }
+  handleTitleChange(value) {
+    this.setState({ title: value });
   }
   /*javi da je botun kliknut*/
   handleClick() {
@@ -97,22 +163,32 @@ class Library extends React.Component {
     if (f) {
       let forma = document.querySelector("form");
       forma.classList.add("transition");
+      this.setState({ button: false });
     }
   }
 
   render() {
+    const kartice = this.state.library;
+    const ispis = kartice.map(book => {
+      return <Card value={book} />;
+    });
     return (
       <div>
-        <Forma />
+        <Forma
+          onTitleChange={this.handleTitleChange}
+          onAuthorChange={this.handleAuthorChange}
+          onPagesChange={this.handlePagesChange}
+          onStatusChange={this.handleStatusChange}
+          onClick={() => this.handleSubmit()}
+        />{" "}
+        {/*onChange={() => this.onTitleChange} */}
         <header>
           <h1>My Library</h1>
         </header>
         <div id="main">
           <Button onClick={() => this.handleClick()} />{" "}
           {/*take control from the child*/}
-          {books.map(book => {
-            return <Card value={book} />;
-          })}
+          {ispis}
         </div>
         {this.renderForm(this.state.button)}
       </div>
